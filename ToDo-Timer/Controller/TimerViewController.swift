10e04c4ff0e0fd741acdc3a5f7e8e629b5312953
Ajith Kusalakumar <ajith_me@outlook.com>
2021-11-10 23:38:31 -0500

    Finished the UI for todo list and timer, just the setting is left now

---
+++ ToDo-Timer/Controller/TimerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TimerViewController.swift
+//  ToDo-Timer
+//
+//  Created by Ajith Kusalakumar on 2021-11-10.
+//
+
+import Foundation
+import UIKit
+class TimerViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
