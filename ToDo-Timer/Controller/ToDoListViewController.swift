a4917fc1bedfbc9761ee7de023c0d4ff0434b96d
Ajith Kusalakumar <ajith_me@outlook.com>
2021-11-22 02:11:12 -0500

    fix to git mistake

--- ToDo-Timer/Controller/ToDoListViewController.swift
+++ ToDo-Timer/Controller/ToDoListViewController.swift
@@ -10,14 +10,31 @@ import UIKit
 class ToDoListViewController: UIViewController {
 
     @IBOutlet weak var toDoTableView: UITableView!
-    
     @IBOutlet weak var toDoCollectionView: UICollectionView!
+    
+    //we want to display all the todo lists at the top
+    //we want a list of todo lists which can be chosen from the top in the collectionview
+    //the tasks in that list will be displayed in the tableview
+    var toDoLists = [ToDoList]()
+    
+    var currentToDoList = 0 //indicates which ToDo list item is selected
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         toDoTableView.dataSource = self
         toDoCollectionView.dataSource = self
+        
+        //going to hard code some todo lists
+        toDoLists.append(ToDoList(name: "Daily"))
+        toDoLists.append(ToDoList(name: "Weekly"))
+        toDoLists.append(ToDoList(name: "Monthly"))
+        toDoLists.append(ToDoList(name: "Project 1"))
+        toDoLists.append(ToDoList(name: "Project 2"))
+
+        toDoCollectionView.register(UINib(nibName: "ToDoListsNameCellCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "collectionToDoLists")
+        toDoCollectionView.reloadData()
+
     }
 
     @IBAction func addButtonPressed(_ sender: Any) {
@@ -44,22 +61,29 @@ extension ToDoListViewController:UITableViewDelegate{
         //
     }
 }
+
+
 extension ToDoListViewController:UICollectionViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return toDoLists.count
 
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionToDoLists", for: indexPath)
-
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionToDoLists", for: indexPath) as! ToDoListsNameCellCollectionViewCell
+        
+        cell.configure(name: toDoLists[indexPath.row].name)
         return cell
     }
 }
 
 extension ToDoListViewController:UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //
+        //once selected display the todo list in the collection view
     }
     
 }
@@ -67,4 +91,5 @@ extension ToDoListViewController:UICollectionViewDelegate{
 //extension  ToDoListViewController:UICollectionViewFlowLayout{
 //
 //}
+
 
